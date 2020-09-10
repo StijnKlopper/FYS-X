72083fcd952fd751782b708b9a7379feb8784ce3
Tarik <33683238+Tarikyil1998@users.noreply.github.com>
2020-09-10 12:09:55 +0200

    Changed something

--- No FYS-X Sky/Assets/GameController.cs
+++ No FYS-X Sky/Assets/GameController.cs
@@ -20,6 +20,7 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Application.OpenURL("https://nl.pornhub.com/view_video.php?viewkey=1202316536");
         //Debug.Log("PLAYERPOSITION : " + player.transform.position + "This is X: " + Mathf.FloorToInt(player.transform.position.x / 10) + " This is Z : " + Mathf.FloorToInt(player.transform.position.z / 10));
         //worldBuilder.loadTiles(player.transform.position);
         //worldBuilder.unloadTiles(player.transform.position);
