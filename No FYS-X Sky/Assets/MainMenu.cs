0d1b9fafedae49e1bd4ba00047c89e260f6dc690
Stan <stanvanweringh@gmail.com>
2020-10-14 14:48:43 +0200

    Added TextMesh package and added a working MainMenuScene

---
+++ No FYS-X Sky/Assets/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    Image loadingImage;
+
+    private void Start()
+    {
+        loadingImage = GameObject.Find("Loading").GetComponent<Image>();
+        loadingImage.enabled = false;
+    }
+
+    public void PlayGame ()
+    {
+        loadingImage.enabled = true;
+
+        // In Unity -> File -> Build Settings are the scene ID's
+        // MainMenuScene is 0
+        // GameScene is 1
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        EditorApplication.isPlaying = false;
+    }
+
+}
