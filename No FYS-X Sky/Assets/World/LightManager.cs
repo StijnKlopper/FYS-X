92a5596e0f975e194342b3d23b50a814b72d1e1b
Stan <stanvanweringh@gmail.com>
2020-10-24 13:37:24 +0200

    Added day and night cycle

---
+++ No FYS-X Sky/Assets/World/LightManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    private Light sun, moon;
+
+    float rotationSpeed = 1f;  // 1f = 6min per cycle
+
+    float xRotationSun, xRotationMoon;
+
+    void Start()
+    {
+        sun = GameObject.Find("Sun").GetComponent<Light>();
+        moon = GameObject.Find("Moon").GetComponent<Light>();
+
+        // Start position for the sun and moon
+        xRotationSun = 20f;
+        xRotationMoon = 180f + xRotationSun;
+    }
+
+    void FixedUpdate()
+    {
+        // Set new rotations
+        xRotationSun += rotationSpeed * Time.deltaTime;
+        xRotationMoon += rotationSpeed * Time.deltaTime;
+        sun.transform.localEulerAngles = new Vector3(xRotationSun, sun.transform.rotation.y, sun.transform.rotation.z);
+        moon.transform.localEulerAngles = new Vector3(xRotationMoon, sun.transform.rotation.y, sun.transform.rotation.z);
+    }
+}
